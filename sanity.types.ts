67a04f45ea4af2418dcad27b94db6ffe291c2950
Ubaid2116 <ukubaid74@gmail.com>
2025-01-27 00:27:07 +0500

    push

--- sanity.types.ts
+++ sanity.types.ts
@@ -13,6 +13,14 @@
  */
 
 // Source: schema.json
+export type SanityImagePaletteSwatch = {
+  _type: "sanity.imagePaletteSwatch";
+  background?: string;
+  foreground?: string;
+  population?: number;
+  title?: string;
+};
+
 export type SanityImagePalette = {
   _type: "sanity.imagePalette";
   darkMuted?: SanityImagePaletteSwatch;
